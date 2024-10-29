4130287f9952a4e9ceec88b5a9fdc282ae62cd24
Ksenia <120771962+kseniaavdonina@users.noreply.github.com>
2024-10-29 11:38:53 +0300

    update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -4,91 +4,125 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+int is_valid_chmod_command(const char *command) {
+    size_t length = strlen(command);
+    for (size_t i = 0; i < length; i++) {
+        if (!(command[i] == 'u' || command[i] == 'g' || command[i] == 'o' || 
+              command[i] == 'a' || command[i] == '+' || command[i] == '-' || 
+              command[i] == 'r' || command[i] == 'w' || command[i] == 'x')) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void apply_symbolic(const char *command, mode_t *current_mode) {
+    char who[10];
+    int add = 0, remove = 0;
+
+    int i = 0;
+    while (command[i]) {
+        int who_ind = 0;
+
+        while (command[i] == 'u' || command[i] == 'g' || command[i] == 'o' || command[i] == 'a') {
+            who[who_ind++] = command[i];
+            i++;
+        }
+
+        if (who_ind == 0) {
+            who[who_ind++] = 'a'; 
+        }
+        who[who_ind] = '\0';
+
+        if (command[i] == '+' || command[i] == '-') {
+            char oper = command[i];
+            i++;
+
+            while (command[i] && (command[i] == 'r' || command[i] == 'w' || command[i] == 'x')) {
+                switch (command[i]) {
+                    case 'r':
+                        for (int j = 0; j < who_ind; j++) {
+                            switch (who[j]) {
+                                case 'u': add |= S_IRUSR; break;
+                                case 'g': add |= S_IRGRP; break;
+                                case 'o': add |= S_IROTH; break;
+                                case 'a': add |= (S_IRUSR | S_IRGRP | S_IROTH); break;
+                            }
+                        }
+                        if (oper == '-') remove |= add;
+                        break;
+                    case 'w':
+                        for (int j = 0; j < who_ind; j++) {
+                            switch (who[j]) {
+                                case 'u': add |= S_IWUSR; break;
+                                case 'g': add |= S_IWGRP; break;
+                                case 'o': add |= S_IWOTH; break;
+                                case 'a': add |= (S_IWUSR | S_IWGRP | S_IWOTH); break;
+                            }
+                        }
+                        if (oper == '-') remove |= add;
+                        break;
+                    case 'x':
+                        for (int j = 0; j < who_ind; j++) {
+                            switch (who[j]) {
+                                case 'u': add |= S_IXUSR; break;
+                                case 'g': add |= S_IXGRP; break;
+                                case 'o': add |= S_IXOTH; break;
+                                case 'a': add |= (S_IXUSR | S_IXGRP | S_IXOTH); break;
+                            }
+                        }
+                        if (oper == '-') remove |= add;
+                        break;
+                }
+                i++;
+            }
+
+            if (oper == '+') {
+                *current_mode |= add;
+            } else if (oper == '-') {
+                *current_mode &= ~remove;
+            }
+
+            add = 0;
+            remove = 0;
+        } else {
+            i++;
+        }
+    }
+}
+
 // Функция для установки прав доступа на основе символьных команд
 void change_mode_symbolic(const char *command, const char *filename) {
     struct stat st;
 
     if (stat(filename, &st) == -1) {
         perror("stat");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    mode_t new_mode = st.st_mode & 0777; // Инициализация новых прав
-    size_t length = strlen(command);
+    mode_t new_mode = st.st_mode & 0777;
+    apply_symbolic(command, &new_mode);
 
-    printf("Текущие права: %o\n", new_mode);
-    printf("Команда: %s\n", command);
-
-    if (strcmp(command, "u-x") == 0) {
-        new_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-    } else if (strcmp(command, "ugo-rwx") == 0) {
-        new_mode = 0; // Убираем все права
-    } else {
-        for (size_t i = 0; i < length; i++) {
-            char who = 'a'; // По умолчанию применяем ко всем
-
-            if (command[i] == 'u' || command[i] == 'g' || command[i] == 'o') {
-                who = command[i];
-                i++;
-            }
-
-            int add = 0, remove = 0;
-            if (command[i] == '+') {
-                add = 1;
-                i++;
-            } else if (command[i] == '-') {
-                remove = 1;
-                i++;
-            }
-
-            // Обрабатываем разрешения
-            while (i < length && (command[i] == 'r' || command[i] == 'w' || command[i] == 'x')) {
-                if (who == 'u' || who == 'a') {
-                    if (command[i] == 'r') new_mode = add ? (new_mode | S_IRUSR) : (new_mode & ~S_IRUSR);
-                    if (command[i] == 'w') new_mode = add ? (new_mode | S_IWUSR) : (new_mode & ~S_IWUSR);
-                    if (command[i] == 'x') new_mode = add ? (new_mode | S_IXUSR) : (new_mode & ~S_IXUSR);
-                }
-                if (who == 'g' || who == 'a') {
-                    if (command[i] == 'r') new_mode = add ? (new_mode | S_IRGRP) : (new_mode & ~S_IRGRP);
-                    if (command[i] == 'w') new_mode = add ? (new_mode | S_IWGRP) : (new_mode & ~S_IWGRP);
-                    if (command[i] == 'x') new_mode = add ? (new_mode | S_IXGRP) : (new_mode & ~S_IXGRP);
-                }
-                if (who == 'o' || who == 'a') {
-                    if (command[i] == 'r') new_mode = add ? (new_mode | S_IROTH) : (new_mode & ~S_IROTH);
-                    if (command[i] == 'w') new_mode = add ? (new_mode | S_IWOTH) : (new_mode & ~S_IWOTH);
-                    if (command[i] == 'x') new_mode = add ? (new_mode | S_IXOTH) : (new_mode & ~S_IXOTH);
-                }
-                i++;
-            }
-        }
-    }
-
-    // Применяем изменения
     if (chmod(filename, new_mode) == -1) {
         perror("chmod");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
-
-    printf("Права успешно изменены на: %o\n", new_mode);
 }
 
 // Функция для установки прав доступа на основе числовых команд
 void change_mode_numeric(const char *command, const char *filename) {
     mode_t mode = (mode_t)strtol(command, NULL, 8);
     
-    // Устанавливаем режим
     if (chmod(filename, mode) == -1) {
         perror("chmod");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
-
-    printf("Права успешно изменены на: %o\n", mode);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Неверное количество аргументов.\n");
-        return EXIT_FAILURE;
+        fprintf(stderr, "Неверное количество аргументов\n");
+        return 1;
     }
 
     const char *command = argv[1];
@@ -99,6 +133,10 @@ int main(int argc, char *argv[]) {
     if (is_numeric) {
         change_mode_numeric(command, filename);
     } else {
+        if (!is_valid_chmod_command(command)) {
+            fprintf(stderr, "Ошибка: неизвестная команда: %s\n", command);
+            return 1;
+        }
         change_mode_symbolic(command, filename);
     }
 
