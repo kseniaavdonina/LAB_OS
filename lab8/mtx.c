05fae10ff76706245b6d87f014c901c1d50c6334
Ksenia <120771962+kseniaavdonina@users.noreply.github.com>
2024-12-05 19:40:52 +0300

    update mtx.c

--- lab8/mtx.c
+++ lab8/mtx.c
@@ -4,28 +4,25 @@
 #include <stdlib.h>
 
 #define READ_THREADS_NUM 10
-#define WRITE_LIMIT 10  // Установим новый предел для записи
+#define WRITE_LIMIT 10
 
 typedef struct {
     int array[100];
-    int next_write;  // Индекс следующей записи
-    pthread_mutex_t mutex; // Мьютекс для синхронизации
+    int next_write;
+    pthread_mutex_t mutex;
 } thread_array;
 
 void* write_thread(void* thread_data) {
     thread_array* shared_array = (thread_array*)thread_data;
     for (int i = 0; i < WRITE_LIMIT; i++) {
-        usleep(10000);  // Задержка для симуляции записи
+        usleep(10000);
         pthread_mutex_lock(&shared_array->mutex);
-        
-        // Записываем данные
+
         shared_array->array[shared_array->next_write] = shared_array->next_write;
         printf("Written: %d\n", shared_array->array[shared_array->next_write]);
-        
-        // Переходим к следующему элементу
+
         shared_array->next_write++;
         usleep(200000);
-        
         pthread_mutex_unlock(&shared_array->mutex);
     }
 
@@ -34,12 +31,12 @@ void* write_thread(void* thread_data) {
 
 void* read_thread(void* thread_data) {
     thread_array* shared_array = (thread_array*)thread_data;
-    usleep(100000);  // Задержка перед стартом чтения
+    usleep(100000);
     for (int i = 0; i < WRITE_LIMIT; i++) {
-        usleep(10000);  // Задержка перед чтением
+        usleep(10000);
         pthread_mutex_lock(&shared_array->mutex);
         if (i < shared_array->next_write) {
-            printf("Read: array[%d] = %d. tid: %lx\n", i, shared_array->array[i], pthread_self());
+            printf("Read: array[%d] = %d tid: %lx\n", i, shared_array->array[i], pthread_self());
         }
         
         pthread_mutex_unlock(&shared_array->mutex);
@@ -56,16 +53,13 @@ int main() {
     pthread_mutex_init(&shared_array.mutex, NULL);
     pthread_t writing_array;
     pthread_t reading_threads[READ_THREADS_NUM];
-
     pthread_create(&writing_array, NULL, write_thread, (void*)&shared_array);
     
     for (int i = 0; i < READ_THREADS_NUM; i++) {
         pthread_create(&reading_threads[i], NULL, read_thread, (void*)&shared_array);
     }
-    // Ожидаем завершения записи
     pthread_join(writing_array, NULL);
     
-    // Ожидаем завершения всех читателей
     for (int i = 0; i < READ_THREADS_NUM; i++) {
         pthread_join(reading_threads[i], NULL);
     }
